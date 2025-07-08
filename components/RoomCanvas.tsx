a3e1ab32ad7bda15240dce71a2102b0e49cffff5
Raghav-28-Gupta <raghavgupta07159@gmail.com>
2025-07-09 01:31:38 +0530

    Refactor WebSocket connection handling in RoomCanvas component for improved clarity and maintainability

--- components/RoomCanvas.tsx
+++ components/RoomCanvas.tsx
@@ -4,50 +4,50 @@ import { useEffect, useRef, useState } from "react";
 import { Canvas } from "./Canvas";
 
 export function RoomCanvas({ roomId }: { roomId: string }) {
-  const [socket, setSocket] = useState<WebSocket | null>(null);
-  const [isOpen, setIsOpen] = useState(false);
+     const [socket, setSocket] = useState<WebSocket | null>(null);
+     const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-  console.log("Attempting WebSocket connection...");
-  const ws = new WebSocket(
-    `${WS_URL}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjFjZTAzNzM1LTBkZWMtNDhmMC05ZDI2LWQyYzJlMDQ2MDBmNCIsImlhdCI6MTc1MTk5MDU1MH0.laD4WyfM0XOCUmuGY9QXq4s9VGHFoe2azas754fNprg`
-  );
+     useEffect(() => {
+          console.log("Attempting WebSocket connection...");
+          const ws = new WebSocket(
+          `${WS_URL}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjFjZTAzNzM1LTBkZWMtNDhmMC05ZDI2LWQyYzJlMDQ2MDBmNCIsImlhdCI6MTc1MTk5MDU1MH0.laD4WyfM0XOCUmuGY9QXq4s9VGHFoe2azas754fNprg`
+          );
 
-  ws.onopen = () => {
-    console.log("WebSocket opened!");
-    setSocket(ws);
-    setIsOpen(true);
-    const data = JSON.stringify({
-      type: "join_room",
-      roomId,
-    });
-    ws.send(data);
-  };
+     ws.onopen = () => {
+     console.log("WebSocket opened!");
+     setSocket(ws);
+     setIsOpen(true);
+     const data = JSON.stringify({
+          type: "join_room",
+          roomId,
+     });
+     ws.send(data);
+     };
 
-  ws.onclose = (event) => {
-    console.warn("WebSocket closed", event);
-    setIsOpen(false);
-    setSocket(null);
-  };
+     ws.onclose = (event) => {
+          console.warn("WebSocket closed", event);
+          setIsOpen(false);
+          setSocket(null);
+     };
 
-  ws.onerror = (err) => {
-    console.error("WebSocket error", err);
-    setIsOpen(false);
-    setSocket(null);
-  };
+     ws.onerror = (err) => {
+          console.error("WebSocket error", err);
+          setIsOpen(false);
+          setSocket(null);
+     };
 
-  return () => {
-    ws.close();
-  };
-}, [roomId]);
+     return () => {
+          ws.close();
+          };
+     }, [roomId]);
 
-  if (!socket || !isOpen) {
-    return <div>Connecting to server....</div>;
-  }
+     if (!socket || !isOpen) {
+          return <div>Connecting to server....</div>;
+     }
 
-  return (
-    <div>
-      <Canvas roomId={roomId} socket={socket} />
-    </div>
+     return (
+     <div>
+          <Canvas roomId={roomId} socket={socket} />
+     </div>
   );
 }
